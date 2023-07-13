e1a9c73a0d44a3230367146f1662cdd3a6f019d4
choipaco <kfckim1014@naver.com>
2023-07-13 17:16:24 +0900

    Update indexJS

--- index.js
+++ index.js
@@ -383,6 +383,7 @@ app.post("/api/email", async(req,res)=>{
       message: '존재하는 이메일 입니다.'
     })
   }
+  
   let rand = "";
   for(let i = 0; i < 6; ++i){
     const a = Math.floor(Math.random() * 10);
